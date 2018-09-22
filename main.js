e9ed11796cc832a52e38233de15af60796d9be65
MCrank <marco_mcse@hotmail.com>
2018-09-22 13:54:54 -0500

    Was so close to getting the decode done. Sad Panda

--- main.js
+++ main.js
@@ -5,13 +5,29 @@ const printToDom = (stringToPrint, elementId) => {
 
 // Encode to UTF-16 function
 // Take the string value in and split it to pass to the charCodeAt() method
-const encodeMe = (stringValue) => {
-  const stringArray = stringValue.split('');
+const encodeMe = (encodeString) => {
+  const stringArray = encodeString.split('');
   let encodedArray = [];
   stringArray.forEach((letter) => {
     encodedArray.push(letter.charCodeAt());
   });
   return encodedArray;
+};
+
+// Decode from UTF-16 function
+// Take Array and decode then put back together
+const decodeMe = (decodeString) => {
+  const stringArray = decodeString.split(',');
+  console.log(stringArray);
+  let decodedString = '';
+  decodedString = String.fromCharCode(stringArray);
+  console.log('Test', decodedString);
+  // :( So close...
+  //   stringArray.forEach((letter) => {
+  //     decodedString.concat(String.fromCharCode(letter));
+  //     // decodedArray.join(String.fromCharCode(letter));
+  //   });
+  return decodedString;
 };
 
 // Get reference to encode button for event
@@ -22,4 +38,14 @@ encodeButton.addEventListener('click', (event) => {
   const encodedString = encodeMe(userInput.value);
 
   printToDom(encodedString, 'encode-output');
+});
+
+// Get reference to decode button for event
+// Create click event listener and send to decode fucntion
+const decodeButton = document.querySelector('#decode-btn');
+decodeButton.addEventListener('click', (event) => {
+  const userInput = document.querySelector('#decode-input');
+  const decodeString = decodeMe(userInput.value);
+
+  printToDom(decodeString, 'decode-output');
 });
