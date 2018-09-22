95ab3f0b6ba8a0b44489fb517526a81e252a2c82
MCrank <marco_mcse@hotmail.com>
2018-09-22 13:24:49 -0500

    Added encode function

--- main.js
+++ main.js
@@ -2,3 +2,24 @@ const printToDom = (stringToPrint, elementId) => {
   const selectedDiv = document.getElementById(elementId);
   selectedDiv.innerHTML = stringToPrint;
 };
+
+// Encode to UTF-16 function
+// Take the string value in and split it to pass to the charCodeAt() method
+const encodeMe = (stringValue) => {
+  const stringArray = stringValue.split('');
+  let encodedArray = [];
+  stringArray.forEach((letter) => {
+    encodedArray.push(letter.charCodeAt());
+  });
+  return encodedArray;
+};
+
+// Get reference to encode button for event
+// Create click event listener and send to encode function
+const encodeButton = document.querySelector('#encode-btn');
+encodeButton.addEventListener('click', (event) => {
+  const userInput = document.querySelector('#encode-input');
+  const encodedString = encodeMe(userInput.value);
+
+  printToDom(encodedString, 'encode-output');
+});
